a67976f255157fa809fbcb9ac4ed0062b0059333
D0erd <seanrichardson98@gmail.com>
2016-07-25 14:25:40 -0700

    Delete DriverTest.java

--- DriverTest.java
+++
@@ -1,6 +0,0 @@
-public class DriverTest {
-    public static void main (String[] args) {
-        WordCounter count = new WordCounter("Hello Hello This is is a test");
-        System.out.println(count.countWords());
-    }
-}
