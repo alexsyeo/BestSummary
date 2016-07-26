6abf5618483243aec35e83b720e7d9a6dd47e61d
Splashman777 <jared@muralt.com>
2016-07-26 11:04:57 -0700

    Added ArticleReceiver and Main class

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+	
+	private static ArrayList<String> articles = new ArrayList<>();
+
+	public static void main(String[] args){
+		//Get 5 articles from CNN News
+		ArticleReceiver receiver = new ArticleReceiver(5 , "http://rss.cnn.com/rss/cnn_topstories.rss");
+		articles = receiver.getArticles();
+    }
+}
