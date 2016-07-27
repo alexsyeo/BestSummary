67c97664254de4a24bbc4f1e85227846b02e34a0
Splashman777 <jared@muralt.com>
2016-07-27 12:52:58 -0700

    Updated Main To Get Multiple News Articles From Different Sources

--- Main.java
+++ Main.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-	
-<<<<<<< HEAD
-	private static List<Article> articles = new ArrayList<Article>();
-=======
-	private static List<Article> articles = new ArrayList<>();
->>>>>>> bc2945c0d621b58b97f8e49783d2f6f3f5962524
 
-	public static void main(String[] args){
-		//Get 5 articles from CNN News
-		ArticleReceiver receiver = new ArticleReceiver(5 , "http://rss.cnn.com/rss/cnn_topstories.rss");
-		articles = receiver.getArticles();
-		System.out.println(articles.get(0));
-    }
+	private static ArrayList<String> articles = new ArrayList<>();
+	public static final String CNN_URL = "http://rss.cnn.com/rss/cnn_topstories.rss";
+	public static final String FOX_URL = "http://feeds.foxnews.com/foxnews/most-popular";
+	public static final String BBC_URL = "http://feeds.bbci.co.uk/news/rss.xml";
+
+	public static void main(String[] args) {
+		// Get top stories from news sources
+
+		// CNN
+		ArticleReceiver receiver1 = new ArticleReceiver(1, CNN_URL);
+		articles.addAll(receiver1.getArticles());
+
+		// FOX
+		ArticleReceiver receiver2 = new ArticleReceiver(1, FOX_URL);
+		articles.addAll(receiver2.getArticles());
+
+		// BBC
+		ArticleReceiver receiver3 = new ArticleReceiver(1, BBC_URL);
+		articles.addAll(receiver3.getArticles());
+	}
 }
