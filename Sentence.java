ea5240b5521463aa3af016b3e54f3af00759781b
Doerd <wadriance@lshigh.org>
2016-07-28 11:10:38 -0700

    Update Sentence.java

--- Sentence.java
+++ Sentence.java
@@ -13,13 +13,27 @@ public class Sentence {
 
   private int points;
   private int numWords;
-  private boolean anaphor;
-  private boolean transition;
+  private int indexInArticle;
   private List<Word> words;
-
+  private String text;
 
   //constructor, creates a sentence that is split up by spaces
   public Sentence(String s) {
+	text = s;
+    String OK = "abcdefghijklmnopqrstuvwxyz' ";
+    for (int i = 0; i < s.length(); i++) {
+        boolean isOK = false;
+        for (int j = 0; j < OK.length(); j++) {
+            if (Character.toLowerCase(s.charAt(i)) == OK.charAt(j))
+                isOK = true;
+            if (isOK)
+                break;
+        }
+        if (!isOK) {
+            s = s.substring(0,i) + s.substring(i+1);
+            i--;
+        }
+    }
     words = new ArrayList<Word>();
     String[] temp = s.split(" ");
     POSTaggerME tagger = setupPOSTagger();
@@ -30,53 +44,89 @@ public class Sentence {
     }
     this.numWords = temp.length;
   }
-  
+
   //sets up the part of speech tagger
   public POSTaggerME setupPOSTagger(){
-	InputStream modelIn = null;
-	POSModel model = null;
-	try {
-	  modelIn = new FileInputStream("/Users/williamadriance/eclipse/workspace/Testing/src/com/company/en-pos-maxent.bin");
-	  model = new POSModel(modelIn);
-	}
-	catch (IOException e) {
-	  // Model loading failed, handle the error
-	  e.printStackTrace();
-	}
-	finally {
-	  if (modelIn != null) {
-	    try {
-	      modelIn.close();
-	    }
-	    catch (IOException e) {
-	    }
-	  }
-	}
-	
-	POSTaggerME tagger = new POSTaggerME(model);
-	
-	return tagger;
+  	InputStream modelIn = null;
+  	POSModel model = null;
+  	try {
+  		
+//William: "/Users/williamadriance/eclipse/workspace/Test/src/bestsummarydevelopment/en-pos-maxent.bin"
+//Alex: "/Users/alex/BestSummary/bestsummarydevelopment/en-pos-maxent.bin"
+//Jared: "/Users/galbraithja/workspace/Test/en-pos-maxent.bin"
+//Sean: "/Users/seanrichardson/BestSummary/src/bestsummarydevelopment/en-pos-maxent.bin"
+  		
+  	  modelIn = new FileInputStream("/Users/seanrichardson/BestSummary/src/bestsummarydevelopment/en-pos-maxent.bin");
+  	  model = new POSModel(modelIn);
+  	}
+  	catch (IOException e) {
+  	  // Model loading failed, handle the error
+  	  e.printStackTrace();
+  	}
+  	finally {
+  	  if (modelIn != null) {
+  	    try {
+  	      modelIn.close();
+  	    }
+  	    catch (IOException e) {
+  	    }
+  	  }
+  	}
+  	POSTaggerME tagger = new POSTaggerME(model);
+
+  	return tagger;
   }
   
-  public boolean scoreSentence(){
-	  for(int i=0;i<words.size();i++){
-		  this.points += words.get(i).getInstances();
-	  }
-	  return true;
+  //sets the score of the sentence
+  public boolean scoreSentence(Article article) {
+    int count = 0;
+    for(int i = 0;i < words.size();i++){
+    	int temp = words.get(i).getInstances()*100;
+    	String posTemp = words.get(i).getPartOfSpeech();
+    	
+      if (posTemp.equals("NNP") || posTemp.equals("NNPS"))
+        temp *= 2;
+      //we use substrings for the rest of the if statements to catch multiple similar parts of speech
+      else if (posTemp.equals("CC") || posTemp.equals("IN") || posTemp.equals("DT") || posTemp.equals("RB"))
+    	temp = 0;
+    
+      count += temp;
+    }
+    //Can adjust this for longer/shorter sentences
+    int ratio = count;
+    //We can tweak this for better results
+    ratio /= checkBadWords() + 1;
+    
+    //changes the score based on the location of the sentence within the article
+    ratio /= (article.getLength() / (article.getLength() - this.indexInArticle));
+    
+    this.setPoints(ratio);
+    return true;
   }
-  
+    
+  //checks to see if there is a "bad" word in the sentence
+	public int checkBadWords() {
+	  int badWords = 0;
+	  if (words.size() >= 6) {
+		  for (int i = 0; i < 6; i++) {
+			String temp = words.get(i).getPartOfSpeech();
+		    if (temp.equals("WP") || (temp.length() > 2 && temp.equals("PRP")))
+		      badWords++;
+		  }
+	  }
+	    return badWords;
+	}
+
+
+  //getters and setters for number of words in the sentence
   public int getNumWords(){
 	  return this.numWords;
   }
+  public void setNumWords(int i) {
+    this.numWords = i;
+  }
 
-  //getters and setters for points
-  public int getPoints() {
-    return this.points;
-  }
-  public void setPoints(int i) {
-    this.points = i;
-  }
-  
+
   //getters and setters for specific Words
   public Word getWord(int index){
 	  return words.get(index);
@@ -87,28 +137,24 @@ public class Sentence {
   }
 
 
-  //getters and setters for anaphors
-  public boolean getAnaphor() {
-    return this.anaphor;
+  //getters and setters for points
+  public int getPoints() {
+    return this.points;
   }
-  public void setAnaphor(boolean i) {
-    this.anaphor = i;
+  public void setPoints(int i) {
+    this.points = i;
+  }
+  
+  public void setIndexInArticle(int i) {
+	  this.indexInArticle = i;
   }
 
-  //getters and setters for transitions
-  public boolean getTransition() {
-    return this.transition;
-  }
-  public void setTransition(boolean i) {
-    this.transition = i;
+  public int getIndexInArticle(){
+	  return this.indexInArticle;
   }
 
   //to-string method
   public String toString() {
-    String j = "";
-    for (int i = 0; i < words.size(); i++) {
-      j += words.get(i) + " ";
-    }
-    return j;
+	  return text;
   }
 }
