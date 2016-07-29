7f84924a925dab9d3e25d4212c8423c7ee124180
alexsyeo <alexsyeo@comcast.net>
2016-07-29 10:03:32 -0700

    Update Sentence.java

--- Sentence.java
+++ Sentence.java
@@ -14,7 +14,9 @@ public class Sentence {
   private int indexInArticle;
   private List<Word> words;
   private static String[] badList = {"cnn", "caption", "photo", "email", "espn", "facebook", "twitter", "pinterest", "whatsapp", "linkedin", "related"};
-
+  private static int NOUN_WEIGHT = 2;
+  private static int PROPER_NOUN_WEIGHT = 4;
+  
   //constructor, creates a sentence that is split up by spaces
   public Sentence(String s, POSModel model) {
 	  words = new ArrayList<Word>();
@@ -33,11 +35,10 @@ public class Sentence {
 //sets the score of the sentence
   public boolean scoreSentence(Article article) {
 	  this.points = instancePoints();
-	  this.points /= (checkBadWords() + 1);
 	  
 	  //changes the score based on the location of the sentence within the article
 	  this.points /= (article.getLength() / (article.getLength() - this.indexInArticle));
-	  if (checkBadList())
+	  if (checkBadList() || this.checkBadWords())
 		  this.points = 0;
 	  return true;
   }
@@ -47,12 +48,15 @@ public class Sentence {
 	    for(int i = 0;i < words.size();i++){
 	    	int temp = words.get(i).getInstances()*100;
 	    	String posTemp = words.get(i).getPartOfSpeech();
-	    	
-	      if (posTemp.equals("NNP") || posTemp.equals("NNPS"))
-	        temp *= 2;
-	      //we use substrings for the rest of the if statements to catch multiple similar parts of speech
-	      else if (posTemp.equals("CC") || posTemp.equals("IN") || posTemp.equals("DT") || posTemp.equals("RB"))
-	    	temp = 0;
+	   
+	    	//multiplies by 2 if the word is a proper noun
+	    	if (posTemp.equals("NNP") || posTemp.equals("NNPS"))
+	    		temp *= PROPER_NOUN_WEIGHT;
+	    	if (posTemp.equals("NN") || posTemp.equals("NNS"))
+	    		temp *= NOUN_WEIGHT;
+	    	//sets the word equal to zero if the word is a coordinating conjunction, subordinating conjunction, preposition, determiner, or adverb
+	    	else if (posTemp.equals("CC") || posTemp.equals("IN") || posTemp.equals("DT") || posTemp.equals("RB"))
+	    		temp = 0;
 	    
 	      count += temp;
 	    }
@@ -60,15 +64,16 @@ public class Sentence {
   }
     
   //checks to see if there is a "bad" word in the sentence
-	public int checkBadWords() {
-	  int badWords = 0;
+	public boolean checkBadWords() {
 	  if (words.size() >= 6) {
 		  for (int i = 0; i < 6; i++) {
-		    if (words.get(i).getPartOfSpeech().equals("WP") || (words.get(i).getPartOfSpeech().length() > 2 && words.get(i).getPartOfSpeech().equals("PRP")))
-		      badWords++;
+			  String sub = words.get(i).getPartOfSpeech();
+			  //checks to see if the first six words in a sentence contain a pronoun or a personal pronoun
+			  if (sub.equals("WP") || (sub.length() > 2 && (sub.equals("WP$") || sub.equals("PRP"))))
+				  return true;
 		  }
 	  }
-	    return badWords;
+	  return false;
 	}
 
 	//checks to see if there is unnecessary info in the sentence
